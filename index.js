3b890bb6cf22fed9a652012e1bd84f806d70cff2
alelulu <ale.silvasuazo@gmail.com>
2019-06-19 23:04:49 -0400

    reading dir and files

--- index.js
+++ index.js
@@ -4,23 +4,36 @@ module.exports = () => {
   // ...
 };
  */
-
-//const mdlinks = require ('./mdlinks.js');
-
+//"Importando" Filesystem de node
 const fs = require('fs');
-const processArgv = process.argv[2]
+//Process no necesita require
+const processArgv = process.argv[2];
 
-// Leyendo archivo de forma asíncrona
-fs.readFile(processArgv, function(err, data) {
-    if(err){
+if(processArgv.includes(".")) {
+  //Leyendo archivo de forma asíncrona
+  fs.readFile(processArgv, function(err, data) {
+    if(err) {
         console.log(err);
     }
-    console.log(data);
-})
-//Leyendo archivo desde directorio
-/* fs.readdir(processArgv, 'utf-8', function(err, data) {
-  if(err){
+    console.log(data.toString());
+  });
+}
+else {
+  //Leyendo archivo desde directorio de forma asíncrona
+  fs.readdir(processArgv, 'utf-8', function(err, data) {
+    if(err) {
       console.log(err);
-  }
-  console.log(data);
-}) */
+    }
+    console.log(data.toString());
+    //data es un array de strings (nombres de los archivos), 
+    //le hago forEach para recorrerlos y les agrego readfile
+    data.forEach(ele => {
+      fs.readFile(processArgv + "\\" + ele, function (err1, data1) {
+        if(err1) {
+          console.log(err1);
+        }
+        console.log(data1.toString());
+      });
+    });
+  });
+};
